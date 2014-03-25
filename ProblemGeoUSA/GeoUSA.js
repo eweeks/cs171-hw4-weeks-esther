4a29a2708d950404a068d4dbe11e48dd91faba1c
Esther Weeks <esta.weeks@gmail.com>
2014-03-25 12:00:02 -0600

    Minor clean up

--- ProblemGeoUSA/GeoUSA.js
+++ ProblemGeoUSA/GeoUSA.js
@@ -164,104 +164,92 @@ function loadStations() {
 									});
 								}
 							}
-					});
-
-					//Use R max, or regular max?
-					
-					var h=200;
-					var w =300;
-					var padding =50;
-					
-					//yScale
-					var yScale = d3.scale.linear().domain([tMax, 0]).range([padding, h]);
-					
-					//yAxis
-					yAxis = d3.svg.axis()
-						.scale(yScale)
-						.orient("left")
-						.ticks(5);
-						
-					//Draw Y axis
-					detailVis.append("g")
-						.attr("class", "axis line graph")
-						.attr("transform", "translate(80, 0)")
-						.call(yAxis)
-						
-					//xScale 
-					var xScale = d3.scale.ordinal()
-						.domain(tKeys.map(function (d) {return d; }))
-						.rangeRoundBands([padding, w], .1);
-						
-					//xScale 
-					var xScale2 = d3.scale.ordinal()
-						.domain(tKeys.map(function (d) {return d; }))
-						.rangeRoundBands([padding, w], .1);
-						
-					//xAxis
-					var xAxis = d3.svg.axis()
-						.scale(xScale)
-						.orient("bottom");
-					
-					//Add X axis
-					var axis = detailVis.append("g")
-						.attr("class", "axis line graph")
-						.attr("transform", "translate(30,"+h+")") 
-						.call(xAxis);
-					
-					//rotates text to angle
-					axis.selectAll("text")
-						.attr("transform", function(d) {
-							return "rotate(-80)translate(-40, -11)";
 						});
+						var h=200;
+						var w =300;
+						var padding =50;
+
+						//yScale
+						//Use max for all stations, or one? Using al stations; tMax
+						var yScale = d3.scale.linear().domain([tMax, 0]).range([padding, h]);
+					
+						//yAxis
+						yAxis = d3.svg.axis()
+							.scale(yScale)
+							.orient("left")
+							.ticks(5);
 						
-					//Draw Bars
-					detailVis.selectAll(".bar")
-						.data(station)
-						.enter().append("rect")
-						.attr("class", "bar graph")
-						.attr("x",  function(d, i) {
-        					return xScale(tKeys[i]);
-    					})
-    					.attr("transform", "translate(10, 0)") 
-						.attr("y", function(d) { 
-							return yScale(d); 
-							
-						})
-						.attr("height", function(d) { 
+						//Draw Y axis
+						detailVis.append("g")
+							.attr("class", "axis line graph")
+							.attr("transform", "translate(80, 0)")
+							.call(yAxis)
+						
+						//xScale 
+						var xScale = d3.scale.ordinal()
+							.domain(tKeys.map(function (d) {return d; }))
+							.rangeRoundBands([padding, w], .1);
+						
+						//xScale 
+						var xScale2 = d3.scale.ordinal()
+							.domain(tKeys.map(function (d) {return d; }))
+							.rangeRoundBands([padding, w], .1);
+						
+						//xAxis
+						var xAxis = d3.svg.axis()
+							.scale(xScale)
+							.orient("bottom");
+					
+						//Add X axis
+						var axis = detailVis.append("g")
+							.attr("class", "axis line graph")
+							.attr("transform", "translate(30,"+h+")") 
+							.call(xAxis);
+					
+						//rotates text to angle
+						axis.selectAll("text")
+							.attr("transform", function(d) {
+								return "rotate(-80)translate(-40, -11)";
+							});
+						
+						//Draw Bars
+						detailVis.selectAll(".bar")
+							.data(station)
+							.enter().append("rect")
+							.attr("class", "bar graph")
+							.attr("x",  function(d, i) {
+								return xScale(tKeys[i]);
+							})
+							.attr("transform", "translate(10, 0)") 
+							.attr("y", function(d) { 
+								return yScale(d);
+							})
+							.attr("height", function(d) { 
 								return (h-yScale(d)); 
-						})
-						.attr("width",  xScale.rangeBand())
-				});
+							})
+							.attr("width",  xScale.rangeBand())
+					});
 			}
-        
-        })
-        console.log(data);
-    });
+		})
+	});
 }
 
-
+//Loads data calls, loadStations
 function loadStats() {
+	d3.json("../data/reducedMonthStationHour2003_2004.json", function(error, data){
+		completeDataSet= data;
+		//sets keys, array of station names
+		keys = Object.keys(data);
+		keys.forEach(function(d, i){
+			//if data has info
+			if(data[d].length>=1){
+				//gets max sum for all stations
+				if(data[d][0].sum > max){
+					max=data[d][0].sum;
+				}
+			}
+		});
 
-    d3.json("../data/reducedMonthStationHour2003_2004.json", function(error, data){
-       // console.log(data[690150][0].sum);
-        completeDataSet= data;
-        keys = Object.keys(data);
-        keys.forEach(function(d, i){
-        	if(data[d].length>=1){
-        	//console.log(data[d][0].sum);
-        		if(data[d][0].sum > max){
-        			max=data[d][0].sum;
-        		}
-        	}
-        	
-        	//data[d][0].map(function(e, j){
-        		
-        	//});
-        });
-        //console.log(max);
-
-		//....
-		
         loadStations();
     });
 
